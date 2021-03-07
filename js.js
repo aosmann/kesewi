d33d511d4cb5adf972c2c20cc507653d96fcd6e2
osman <aosmannur@gmail.com>
2021-03-07 12:59:32 +0200

    ss

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 
 
 let mouseCursor = document.querySelector (".cursor")
-let navLinks = document.querySelectorAll (".navbar")
+let navLinks = document.querySelectorAll (".navbar, .footer")
 let hero = document.querySelectorAll (".brand-text-2")
 
 
@@ -16,6 +16,16 @@ function cursor(e){
 
 navLinks.forEach(link =>{
     link.addEventListener ('mouseleave', () => {
+        mouseCursor.classList.remove ('link-grow-dark')
+    });
+    link.addEventListener ('mouseover', () => {
+        mouseCursor.classList.add ('link-grow-dark')
+    });
+});
+
+
+hero.forEach(link =>{
+    link.addEventListener ('mouseleave', () => {
         mouseCursor.classList.remove ('link-grow')
     });
     link.addEventListener ('mouseover', () => {
@@ -24,14 +34,36 @@ navLinks.forEach(link =>{
 });
 
 
-hero.forEach(link =>{
-    link.addEventListener ('mouseleave', () => {
-        mouseCursor.classList.remove ('link-grow-hero')
+
+let highlightElements = document.querySelectorAll('.highlight');
+let highlightElementsYellow = document.querySelectorAll('.highlight-yellow');
+let highlightElementsDark = document.querySelectorAll('.highlight-dark');
+
+
+
+highlightElements.forEach(highlightElement => {
+    highlightElement.addEventListener('mouseover',() => {
+      mouseCursor.style.opacity = 0;
     });
-    link.addEventListener ('mouseover', () => {
-        mouseCursor.classList.add ('link-grow-hero')
+    highlightElement.addEventListener('mouseleave',() => {
+      mouseCursor.style.opacity = 1;
     });
-});
+  });
 
+  highlightElementsYellow.forEach(highlightElementYellow => {
+    highlightElementYellow.addEventListener('mouseover',() => {
+      mouseCursor.style.opacity = 0;
+    });
+    highlightElementYellow.addEventListener('mouseleave',() => {
+      mouseCursor.style.opacity = 1;
+    });
+  });
 
-
+  highlightElementsDark.forEach(highlightElementDark => {
+    highlightElementDark.addEventListener('mouseover',() => {
+      mouseCursor.style.opacity = 0;
+    });
+    highlightElementDark.addEventListener('mouseleave',() => {
+      mouseCursor.style.opacity = 1;
+    });
+  });
